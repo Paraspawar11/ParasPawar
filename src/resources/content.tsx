dc7ea0a9bec61ebfed033708ee59d5c738664835
Paraspawar11 <pawarparas79@gmail.com>
2026-03-04 00:59:16 +0530

    Fix: Update location to valid IANATimeZone for build

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -8,7 +8,7 @@ const person: Person = {
   role: "Developer @ NexShell",
   avatar: "/images/avatar.png",
   email: "nexshellofficial@proton.me",
-  location: "Thane, Maharashtra", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  location: "Asia/Kolkata", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: [], // optional: Leave the array empty if you don't want to display languages
 };
 
